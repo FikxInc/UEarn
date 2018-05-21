0df96c4fc344fbb8c6dcb284654025d37b8d5c81
kOFI CHARLES <charlesbasoah93@gmail.com>
2018-05-21 10:11:25 +0000

    refactoring

--- app/src/main/java/com/example/phiiphiroberts/uearn/Tabs/MyAdapter.java
+++ app/src/main/java/com/example/phiiphiroberts/uearn/Tabs/MyAdapter.java
@@ -1,4 +1,71 @@
 package com.example.phiiphiroberts.uearn.Tabs;
 
-public class MyAdapter {
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
+
+import com.example.phiiphiroberts.uearn.R;
+import com.example.phiiphiroberts.uearn.fragment.DownlinersFragment;
+import com.example.phiiphiroberts.uearn.fragment.HomeFragment;
+import com.example.phiiphiroberts.uearn.fragment.ProfileFragment;
+import com.example.phiiphiroberts.uearn.fragment.ShopingFragment;
+import com.example.phiiphiroberts.uearn.fragment.TransactionFragment;
+
+
+public class MyAdapter extends FragmentPagerAdapter {
+    private Context mContext;
+    private String[] titles = {"Home", "Downliners", "Transactions", "Profile"};
+    int[] icon = new int[]{R.drawable.baseline_home_white_24dp, R.drawable.baseline_trending_down_white_24dp, R.drawable.baseline_work_outline_white_24dp, R.drawable.baseline_perm_identity_white_24dp};
+    private int heightIcon;
+
+    public MyAdapter(FragmentManager fm, Context c) {
+        super(fm);
+        mContext = c;
+        double scale = c.getResources().getDisplayMetrics().density;
+        heightIcon = (int) (24 * scale + 0.5f);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment frag = null;
+
+        if (position == 0) {
+            frag = new HomeFragment();
+        } else if (position == 1) {
+            frag = new DownlinersFragment();
+        } else if (position == 2) {
+            frag = new TransactionFragment();
+        } else if (position == 3) {
+            frag = new ProfileFragment();
+        }
+
+
+        Bundle b = new Bundle();
+        b.putInt("position", position);
+        frag.setArguments(b);
+        return frag;
+    }
+
+    @Override
+    public int getCount() {
+        return titles.length;
+    }
+
+    public CharSequence getPageTitle(int position) {
+        Drawable d = mContext.getResources().getDrawable(icon[position]);
+        d.setBounds(0, 0, heightIcon, heightIcon);
+        ImageSpan is = new ImageSpan(d);
+
+        SpannableString sp = new SpannableString(" ");
+        sp.setSpan(is, 0, sp.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        return sp;
+    }
 }
