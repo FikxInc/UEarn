3a3d0a7f866f0264bddc83c32814f06429ead246
kOFI CHARLES <charlesbasoah93@gmail.com>
2018-05-21 18:06:42 +0000

    refactoring

--- app/src/main/java/com/example/phiiphiroberts/uearn/MainActivity.java
+++ app/src/main/java/com/example/phiiphiroberts/uearn/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.phiiphiroberts.uearn;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.Button;
@@ -21,10 +23,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
 public class MainActivity extends AppCompatActivity {
 
+    MaterialSearchView searchView;
     private SlidingTabLayout mSlidingTabLayout;
     private ViewPager mViewPager;
 
@@ -35,19 +39,46 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("UEARN SEARCH");
+        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
 
+        //SEARCH VIEW BAR
+         searchView = findViewById(R.id.search_view);
+         searchView.closeSearch();
+         searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+             @Override
+             public boolean onQueryTextSubmit(String query) {
+                 //HERE CREATE YOUR FILTERING
+                 return false;
+             }
+
+             @Override
+             public boolean onQueryTextChange(String newText) {
+                 //REAL TIME CHANGE IF YOU ARE TYPING HERE
+                 return false;
+             }
+         });
+
+
+        // SLIDING NAVIGATION TABS
         mViewPager = findViewById(R.id.vp_tabs);
         mViewPager.setAdapter(new MyAdapter(getSupportFragmentManager(),this));
 
         mSlidingTabLayout = findViewById(R.id.stl_tabs);
         mSlidingTabLayout.setDistributeEvenly(true);
         mSlidingTabLayout.setBackgroundColor(getResources().getColor(R.color.login));
-        mSlidingTabLayout.setSelectedIndicatorColors(getResources().getColor(R.color.gradStart));
+        mSlidingTabLayout.setSelectedIndicatorColors(getResources().getColor(R.color.white));
         mSlidingTabLayout.setCustomTabView(R.layout.tab_view, R.id.tv_tab);
         mSlidingTabLayout.setViewPager(mViewPager);
 
-
     }
 
-
+    //SEARCH VIEW BAR
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item, menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+        return true;
+    }
 }
